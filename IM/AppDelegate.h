f5977724282618d867d19c340a2117e607d6e1d6
Wang <wangNB@wangdemacbook-air.local>
2016-09-06 14:43:21 +0800

    Initial Commit

---
+++ IM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IM
+//
+//  Created by Wang on 16/9/6.
+//  Copyright © 2016年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
